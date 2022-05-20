aaaeacf4f29a1535e5b91e27f7e571937097c9da
Jobayer-ak <jobayer.ak@gmail.com>
2022-05-21 02:10:41 +0600

    all warning are fixed

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { format } from "date-fns";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
 
 const BookingModal = ({ date, treatment, setTreatment }) => {
   const { _id, name, slots } = treatment;
+  const [user, loading, error] = useAuthState(auth);
 
   // handle booking
   const handleBooking = (event) => {
@@ -38,21 +41,25 @@ const BookingModal = ({ date, treatment, setTreatment }) => {
             <select
               name="slot"
               className="select select-bordered w-full max-w-xs">
-              {slots.map((slot) => (
-                <option value={slot}>{slot}</option>
+              {slots.map((slot, index) => (
+                <option key={index} value={slot}>
+                  {slot}
+                </option>
               ))}
             </select>
             <input
               name="name"
               type="text"
-              placeholder="Your Name"
+              value={user?.displayName || ""}
               className="input input-bordered w-full max-w-xs"
+              readOnly
             />
             <input
               name="email"
               type="email"
-              placeholder="Your Email Address"
+              value={user?.email || ""}
               className="input input-bordered w-full max-w-xs"
+              readOnly
             />
             <input
               name="phone"
