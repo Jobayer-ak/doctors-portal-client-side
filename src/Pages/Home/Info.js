d02758e116eef3c4b2004860ed5ba39bdd0d3004
Jobayer-ak <jobayer.ak@gmail.com>
2022-05-20 18:34:12 +0600

    info.js page is fixed

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -6,20 +6,18 @@ import phone from "../../assets/icons/phone.svg";
 
 const Info = () => {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 text-white">
+    <div className="grid grid-cols-1 lg:grid-cols-3 gap-5">
       <InfoCard
         cardTitle="Opening Hours"
-        bgclassName="font-bold bg-gradient-to-r from-secondary to-primary"
+        bgClass="bg-gradient-to-r from-secondary to-primary"
         img={clock}></InfoCard>
-
       <InfoCard
         cardTitle="Our Locations"
-        bgclassName="font-bold bg-neutral"
+        bgClass="bg-neutral"
         img={marker}></InfoCard>
-
       <InfoCard
         cardTitle="Contact Us"
-        bgclassName="font-bold bg-gradient-to-r from-secondary to-primary"
+        bgClass="bg-gradient-to-r from-secondary to-primary"
         img={phone}></InfoCard>
     </div>
   );
--- src/Pages/Home/InfoCard.js
+++ src/Pages/Home/InfoCard.js
@@ -6,7 +6,7 @@ const InfoCard = ({ img, cardTitle, bgClass }) => {
       <figure className="pl-5 pt-5">
         <img src={img} alt="Album" />
       </figure>
-      <div className="card-body">
+      <div className="card-body text-white">
         <h2 className="card-title">{cardTitle}</h2>
         <p>Click the button to listen on Spotiwhy app.</p>
       </div>
